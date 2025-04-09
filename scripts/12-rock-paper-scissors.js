6092f96e55de6fa93e280e8577ed4e42358a5866
tjeremiah <taurel107@gmail.com>
2025-04-09 07:50:48 -0400

    completed lesson 12

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -33,9 +33,34 @@ function autoPlay() {
      clearInterval(intervalId);
      isAutoPlaying = false;
   }
-  
-  
 }
+
+document.querySelector('.js-rock-button')
+  .addEventListener('click', () => {
+  playGame('rock');
+})
+
+document.querySelector('.js-paper-button')
+  .addEventListener('click', () => {
+  playGame('paper');
+})
+
+document.querySelector('.js-scissors-button')
+  .addEventListener('click', () => {
+  playGame('scissors');
+})
+
+//play the game when a key is pressed on the keyboard
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r' ) {
+    playGame('rock');
+
+  }  else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors')
+  }
+})
 
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -1,5 +1,6 @@
 const todoList = [{
-   name:'make dinner', dueDate: '2022-12-22'}, { 
+   name:'make dinner', dueDate: '2022-12-22'}, 
+   { 
    name: 'wash dishes', dueDate: '2022-12-22'
   }];
 //display list
@@ -11,20 +12,29 @@ function renderTodoList() {
   todoList.forEach(function(todoObject, index){
     const {name, dueDate} = todoObject;
     const html = `
-    
       <div>${name}</div> 
       <div>${dueDate}</div>
-      <button onclick="
-        todoList.splice(${index}, 1);
-        renderTodoList();
-      " class = "delete-todo-button">Delete</button>   
+      <button class="delete-todo-button js-delete-todo-button">Delete</button>   
   `;
    todoListHTML += html;
  });
   
   document.querySelector('.js-todo-list')
   .innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      })
+    })
 }
+
+document.querySelector('.js-add-todo-button')
+  .addEventListener('click', () => {
+    addTodo();
+  })
 
 function addTodo() {
   let inputElement = document.querySelector('.js-name-input');
